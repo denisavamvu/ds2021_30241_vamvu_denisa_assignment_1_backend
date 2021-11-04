8f1486367189be9da57c0762094be81dd9a1105d
denisavamvu <vamvudenisa@gmail.com>
2021-11-04 09:02:47 +0200

    allow index controller

--- src/main/java/ro/tuc/ds2020/config/WebSecurityConfig.java
+++ src/main/java/ro/tuc/ds2020/config/WebSecurityConfig.java
@@ -43,6 +43,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.cors().and().csrf().disable()
                 .authorizeRequests().antMatchers("/authenticate").permitAll()
+                .antMatchers("/").permitAll()
                 .mvcMatchers(HttpMethod.POST, "/addAdmin").permitAll().
                 anyRequest().authenticated().and().
                 exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
